b4af3c095b6a8352944a466ccb6205e91eb06c45
miso-devel <kengo279@icloud.com>
2023-08-13 23:55:35 +0900

    313b

--- ABC/abc313/src/bin/b.rs
+++ ABC/abc313/src/bin/b.rs
@@ -2,5 +2,29 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n:usize,
+        m:usize,
+        xy:[(usize,usize);m],
+    }
+
+    let mut is_lose = vec![false; n];
+    for now in xy {
+        is_lose[now.1 - 1] = true;
+    }
+
+    let loser_count = is_lose.iter().filter(|&&x| !x).count();
+    if loser_count != 1 {
+        println!("-1");
+        return;
+    }
+
+    let loser = is_lose
+        .iter()
+        .enumerate()
+        .filter(|&x: &(usize, &bool)| !x.1)
+        .collect::<Vec<_>>()[0]
+        .0;
+
+    println!("{:?}", loser + 1);
 }
