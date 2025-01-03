1073545c500b8019aed9188bd374c8b157f168c8
miso-devel <kengo279@icloud.com>
2025-01-03 19:17:04 +0900

    abc 006 b

--- ABC/abc006/src/bin/b.rs
+++ ABC/abc006/src/bin/b.rs
@@ -2,5 +2,21 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize
+    }
+    let mut nums: [usize; 3] = [0, 0, 1];
+
+    if n <= 3 {
+        println!("{}", nums[n - 1]);
+        return;
+    }
+    let mut last_num: usize = 1;
+    for _ in 4..=n {
+        last_num = (nums[0] + nums[1] + nums[2]) % 10007;
+        nums[0] = nums[1];
+        nums[1] = nums[2];
+        nums[2] = last_num;
+    }
+    println!("{}", last_num);
 }
