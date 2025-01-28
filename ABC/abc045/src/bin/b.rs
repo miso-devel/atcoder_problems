fa453eb0e39efb0ade99fe00c1fbf04821168d2f
miso-devel <kengo279@icloud.com>
2025-01-28 23:47:18 +0900

    add 045 abc

--- ABC/abc045/src/bin/b.rs
+++ ABC/abc045/src/bin/b.rs
@@ -8,23 +8,29 @@ fn main() {
         b:String,
         c:String
     }
-    let mut hash: HashMap<&str, Vec<char>> = HashMap::new();
-    let mut current: &str = "a";
-    hash.insert("a", a.chars().collect::<Vec<char>>());
-    hash.insert("b", b.chars().collect::<Vec<char>>());
-    hash.insert("c", c.chars().collect::<Vec<char>>());
-    loop {
-        let current_hash = hash[current].clone();
-        let count = current_hash.len();
-        if count == 0 {
+    let mut hash: HashMap<String, Vec<char>> = HashMap::new();
+
+    let mut current: String = "a".to_string();
+    hash.insert("a".to_string(), a.chars().collect::<Vec<char>>());
+    hash.insert("b".to_string(), b.chars().collect::<Vec<char>>());
+    hash.insert("c".to_string(), c.chars().collect::<Vec<char>>());
+
+    for _ in 0..=100 {
+        if hash[&current].is_empty() {
             break;
         }
-
-        let mut next = current_hash[0];
-        if next == 'a' {
-            current = "a";
-        }
+        let mut current_hand: Vec<char> = hash[&current].clone();
+        let popped: char = current_hand.pop().unwrap();
+        hash.insert(current, current_hand);
+        let next_str: String = popped.to_string();
+        current = next_str;
     }
 
-    println!("{:?}", hash);
+    if current == "a" {
+        println!("A");
+    } else if current == "b" {
+        println!("B");
+    } else if current == "c" {
+        println!("C");
+    }
 }
