09bad8f3a95392b06d5dedb158a5a3c15f8ff357
miso-devel <kengo279@icloud.com>
2025-02-02 19:51:38 +0900

    abc045 b

--- ABC/abc045/src/bin/b.rs
+++ ABC/abc045/src/bin/b.rs
@@ -1,5 +1,4 @@
 use proconio::{fastout, input};
-use std::collections::HashMap;
 
 #[fastout]
 fn main() {
@@ -8,29 +7,25 @@ fn main() {
         b:String,
         c:String
     }
-    let mut hash: HashMap<String, Vec<char>> = HashMap::new();
+    let strings = [a, b, c];
+    let mut now = [0, 0, 0];
+    let mut current = 0;
 
-    let mut current: String = "a".to_string();
-    hash.insert("a".to_string(), a.chars().collect::<Vec<char>>());
-    hash.insert("b".to_string(), b.chars().collect::<Vec<char>>());
-    hash.insert("c".to_string(), c.chars().collect::<Vec<char>>());
-
-    for _ in 0..=100 {
-        if hash[&current].is_empty() {
-            break;
-        }
-        let mut current_hand: Vec<char> = hash[&current].clone();
-        let popped: char = current_hand.pop().unwrap();
-        hash.insert(current, current_hand);
-        let next_str: String = popped.to_string();
-        current = next_str;
+    while now[current] < strings[current].len() {
+        let current_char = strings[current].chars().nth(now[current]).unwrap();
+        now[current] += 1;
+        current = match current_char {
+            'a' => 0,
+            'b' => 1,
+            'c' => 2,
+            _ => 0,
+        };
     }
-
-    if current == "a" {
-        println!("A");
-    } else if current == "b" {
-        println!("B");
-    } else if current == "c" {
-        println!("C");
-    }
+    let result = match current {
+        0 => "A",
+        1 => "B",
+        2 => "C",
+        _ => unreachable!(),
+    };
+    println!("{}", result);
 }
