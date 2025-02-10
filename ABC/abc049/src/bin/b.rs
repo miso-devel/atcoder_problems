933f808ab8746575bd43c549bb6c2279375150cc
miso-devel <kengo279@icloud.com>
2025-02-10 23:55:23 +0900

    add b

--- ABC/abc049/src/bin/b.rs
+++ ABC/abc049/src/bin/b.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        h:i32,
+        _:i32,
+        c:[String;h]
+    }
+    let mut result = String::new();
+    for i in 0..h {
+        result.push_str(&c[i as usize]);
+        result.push('\n');
+        result.push_str(&c[i as usize]);
+        if i != h - 1 {
+            result.push('\n');
+        }
+    }
+    println!("{}", result);
 }
